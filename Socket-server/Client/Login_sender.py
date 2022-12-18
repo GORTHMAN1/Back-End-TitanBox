431529b74b8c51604ebf138a8f74dadb03c1ba92
Doru991 <dorurogoveanu123@gmail.com>
2022-12-18 22:29:39 +0200

    Operation system and registration

--- Socket-server/Client/Login_sender.py
+++ Socket-server/Client/Login_sender.py
@@ -3,17 +3,19 @@ import  socket
 HEADER = 64
 PORT = 5050
 FORMAT = "utf-8"
-DISCONNECT_MSG = "!DISCONNECT"
+DISCONNECT_OP=255
+LOGIN_OP=0
+REGISTER_OP=1
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
-def send(msg):
+def send(msg, op):
     message = msg.encode(FORMAT)
     msg_lenght = len(message)
-    send_length = str(msg_lenght).encode(FORMAT)
+    send_length = str(msg_lenght).encode(FORMAT) + b':' + str(op).encode(FORMAT)
     send_length += b' ' * (HEADER - len(send_length))
     client.send(send_length)
     client.send(message)
@@ -21,8 +23,9 @@ def send(msg):
 
 user=input("User:")
 passwd=input("Parola:")
-send(f'{user}:{passwd}')
-send(DISCONNECT_MSG)
+send(f'{user}:{passwd}', REGISTER_OP)
+send(f'{user}:{passwd}', LOGIN_OP)
+send("DISCONNECT", DISCONNECT_OP)
 
 
 
